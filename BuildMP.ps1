d4b41dd34b6ccf14bd3a0f445939f6952c252065
Brent Wright <brentgwright@gmail.com>
2020-01-30 07:19:06 -0500

    Much improved XML merge
    XPaths used for merging child nodes.
    Warnings added for missed nodes in fragments.

--- BuildMP.ps1
+++ BuildMP.ps1
@@ -24,14 +24,40 @@ $here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $MPBuildDir = "$here\Management Packs"
 $outputDir = "$here\Output"
 
+# A list of paths where child nodes will be merged into a single MP.
+$xmlChildPaths = @(
+    "Manifest"
+    "TypeDefinitions/EntityTypes/ClassTypes"
+    "TypeDefinitions/EntityTypes/RelationshipTypes"
+    "TypeDefinitions/ModuleTypes"
+    "Monitoring/Discoveries"
+    "Monitoring/Monitors"
+    "Monitoring/Tasks"
+    "LanguagePacks/LanguagePack/DisplayStrings"
+    "LanguagePacks/LanguagePack/KnowledgeArticles"
+)
+
 
 #endregion  User Configuration
+
+# helper function to recursively return the deepest node paths
+function Get-XMLChildNodePath {
+    param ($XMLNode, $CurrentPath = "")
+
+    if ($xmlnode.hasChildNodes) {
+        foreach ($node in $xmlnode.childnodes) {
+            Get-XMLChildNodePath -xmlNode $node -currentPath $("$currentPath/{0}" -f $node.localname)
+        }
+    } else {
+        $CurrentPath
+    }
+}
 
 
 
 $managementPacks = Get-ChildItem $MPBuildDir
 
-    
+
 if ( -not $(Test-Path $outputDir) ) {
     $null = New-Item $outputDir -ItemType Directory -Force
 }
@@ -43,33 +69,79 @@ foreach ($mp in $managementPacks) {
 
     # Read in a copy of the MP Template.
     [xml]$mpXML = Get-Content "$here\Templates\ManagementPack.xml"
-    
-    # Get a list of .mpx files in the MP project.
+
+    # Get a list of .mpx files in the MP project, along with the manifest.mpx.
     $fragments = @(Get-ChildItem -Path $mp.fullname -Filter "*.mpx" -Recurse)
 
     "{0}: Fragments to add: {1}" -f $mp.Name, $fragments.count | Write-Host
 
-    # Process each .mpx file
+    # Process each .mpx file, including the manifest.mpx
     foreach ($fragment in $fragments) {
 
-        "  Fragment file: {0}" -f $fragment.name | Write-Host
+        "  Fragment file: {0}" -f $fragment.name | Write-Host -ForegroundColor Green
 
         [xml]$fragXML = Get-Content $fragment.fullname
 
-        # Ignore comments and empty childnodes
-        $eligibleSections = $fragXML.ManagementPackFragment.ChildNodes | 
-            Where-Object {$_.localname -ne "#comment" -and $_.haschildnodes}
+        # Build a list of all leaf nodes.
+        # Items will be removed as they are merged.
+        # At the end, any items left will be reported as a warning.
+        $fragXMLLeafNodes = Get-XMLChildNodePath -XMLNode $fragXML.ManagementPackFragment
 
-        # Process each section of the manifest fragment separately.
-        foreach ($section in $eligibleSections.localname) {
-
-            "    {0}: Merging section: {1}" -f $fragment.name, $section.localname
-
-            foreach ($childNode in $fragXML.SelectNodes("ManagementPackFragment/$section").ChildNodes) {
-            $mpXML.SelectNodes("/ManagementPack/$section").AppendChild($mpXML.ImportNode($childNode.clone(),$true)) | Out-Null
+        # Build a list of XPaths that have children in XML fragment.
+        $eligibleXPaths = foreach ($xmlChildPath in $xmlChildPaths) {
+            if ( $fragXML.SelectNodes("ManagementPackFragment/$xmlChildPath").count -gt 0 ) {
+                $xmlChildPath
             }
         }
-    }
+
+        # Perform the merge into the MP XML document.
+        foreach ($XPath in $eligibleXPaths) {
+
+            "    Adding nodes from XPath: $XPath" | Write-Host
+
+            # Check if the XPath exists in MP XML.
+            if ($mpXML.SelectNodes("ManagementPack/$XPath").count -gt 0) {
+
+                foreach ($childNode in $fragXML.SelectNodes("ManagementPackFragment/$xpath").ChildNodes) {
+                    $mpXML.SelectNodes("/ManagementPack/$XPath").AppendChild($mpXML.ImportNode($childNode.clone(),$true)) | Out-Null
+                }
+
+                # Remove any items that were merged.
+                $fragXMLLeafNodes = $fragXMLLeafNodes | Where-Object {$_ -notlike "/$XPath*"}
+
+            # Otherwise, find the deepest node that does exist, and merge from there.
+            } else {
+
+                # Move backwards in XPath until node exists in MP XML, then add node 1 level deeper.
+                $nodeNotExist = $true
+                while ($nodeNotExist) {
+
+                    # Keep the XPath that is 1 level deeper, to use when parent node does exist.
+                    $childXPath = $XPath
+
+                    $XPath = $(Split-Path -Path $XPath -Parent) -replace "\\","/"
+
+                    "      XPath doesn't exist, trimming to: $XPath"
+
+                    if ($mpXML.SelectNodes("ManagementPack/$XPath").count -gt 0) {
+                        $nodeNotExist = $false
+                    }
+                }
+
+                $childNode = $fragXML.SelectNodes("ManagementPackFragment/$childXPath")
+                $mpXML.SelectNodes("/ManagementPack/$XPath").AppendChild($mpXML.ImportNode($childNode.clone(),$true)) | Out-Null
+
+                "      ChildNode `"{0}`" added to parent: {1}" -f $(split-path $childXPath -Leaf), $XPath | Write-Host
+
+                # Remove any items that were merged.
+                $fragXMLLeafNodes = $fragXMLLeafNodes | Where-Object {$_ -notlike "/$childXPath*"}
+            }
+        } # Foreach: XPath
+
+        if ($fragXMLLeafNodes.count -gt 0) {
+            $fragXMLLeafNodes | ForEach-Object {"Fragment node not merged: $_"} | Write-Warning
+        }
+    } # Foreach: Fragment
 
     $mpXML.Save("$outputDir\$($mp.Name).xml")
 }
